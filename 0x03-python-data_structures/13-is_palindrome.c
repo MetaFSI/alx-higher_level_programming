6283349ecf59901cab523f9f4e4ba5d5b9d09ecf
Mounir elfahssi <elfahssimounir7@gmail.com>
2023-06-12 10:49:09 -0700

    task 6 - 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,78 @@
+/*
+ * File: 13-is_palindrome.c
+ * Auth: mounir el fahssi
+ */
+
+#include "lists.h"
+
+listint_t *reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
+
+/**
+ * reverse_listint - Reversing single link listint_t list
+ * @head: pointer -> starting node => list to reverse
+ *
+ * Return: pointer-> headi => reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
+
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
+
+/**
+ * is_palindrome - Checking if singl link listed is palindrome
+ * @head: pointer to heading of link list
+ *
+ * Return: in case linked list is !are palindrome 0.
+ *         in case linked list are palindrome 1.
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *tmp, *rev, *mid;
+	size_t size = 0, i;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+
+	tmp = *head;
+	while (tmp)
+	{
+		size++;
+		tmp = tmp->next;
+	}
+
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		tmp = tmp->next;
+
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
+		return (0);
+
+	tmp = tmp->next->next;
+	rev = reverse_listint(&tmp);
+	mid = rev;
+
+	tmp = *head;
+	while (rev)
+	{
+		if (tmp->n != rev->n)
+			return (0);
+		tmp = tmp->next;
+		rev = rev->next;
+	}
+	reverse_listint(&mid);
+
+	return (1);
+}
+
