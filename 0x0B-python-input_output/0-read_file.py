8f59b28b90e8c57bd3c2867cb9268144eff60c3a
Mounir elfahssi <elfahssimounir7@gmail.com>
2023-07-11 18:05:03 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Defining a text file-reading"""
+
 
 def read_file(filename=""):
+    """Printing contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
